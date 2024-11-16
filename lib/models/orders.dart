cb6d3cec2c260dd749cfc16c87a6d8c38a7169fc
El Hassane Boushib <boushib.elhassane@gmail.com>
2024-11-15 20:36:01 -0500

    Remove unused imports

--- lib/models/orders.dart
+++ lib/models/orders.dart
@@ -1,8 +1,6 @@
-import "dart:convert";
 import "package:flutter/foundation.dart";
 import "package:shop/models/cart_item.dart";
 import "package:shop/models/order.dart";
-import "package:http/http.dart" as http;
 import "package:shop/services/mongo.dart";
 
 class Orders with ChangeNotifier {
